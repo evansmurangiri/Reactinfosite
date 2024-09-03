152c3476f5c53d513bb0cd534874d5eb65724cd1
evansmurangiri <94174746+evansmurangiri@users.noreply.github.com>
2024-09-03 23:49:29 +0300

    initial commit

---
+++ infosite/src/components/navigation.tsx
@@ -0,0 +1,14 @@
+
+function Navigation(){
+    return(
+        <>
+        <nav className="nav-bar">
+            <img src="../images/react.png" />
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+        </>
+    )
+}
+
+export default Navigation;
